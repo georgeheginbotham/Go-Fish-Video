822517dffaa79faebff02505ef0cb8a5a495cc14
George Heginbotham <68346866+georgeheginbotham@users.noreply.github.com>
2021-07-11 22:59:59 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,14 @@
+ new WOW().init();
+});
+/* ========================================================================= */
+/*	Page Preloader
 window.addEventListener('scroll', function(e) {
 
             if( $(window).scrollTop() <= 50) {
                 $('.wow').removeClass('animated');
                 $('.wow').removeAttr('style');
-                new WOW().init();
+
             }
-});
-/* ========================================================================= */
-/*	Page Preloader
 /* ========================================================================= */
 
 $(window).on('load', function () {
